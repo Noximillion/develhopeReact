a01e383e6f1dbeba1ccb7783a905491a241fa5f4
Andre <aanuta9997@gmail.com>
2023-05-27 19:37:28 +0200

    exercise-1 about react

---
+++ develhope-app/src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Hello } from "./hello";
+import { Message } from "./message";
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Hello />
+                <Message />
+            </div>
+        )
+    }
+}
