d9c958c43d7eca85896d64a8bb3d40eab053eef5
Andre <aanuta9997@gmail.com>
2023-05-28 18:47:35 +0200

    exercise 5

--- develhope-app/src/app.js
+++ develhope-app/src/app.js
@@ -4,6 +4,8 @@ import { Welcome } from "./welcome";
 
 export class App extends React.Component {
     render() {
-        return <Welcome name="pino" age={23}/>
+        return <Welcome name={<strong>Pino</strong>} age={23}/>
     }
 }
+
+// per passarlo come jsx bisogna inserirlo fra le parentesi graffe, affinché venga interpretato come un jsx
