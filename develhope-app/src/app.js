72b3cf0ad53efc1035bde5cb0ad1091daff9fa4f
Andre <aanuta9997@gmail.com>
2023-05-30 11:52:19 +0200

    esercizio 12

--- develhope-app/src/app.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Welcome } from "./welcome";
-
-
-export class App extends React.Component {
-    render() {
-        return <Welcome name='John' age={19}/>
-    }
-}
---
+++ develhope-app/src/counter.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export class Counter extends React.Component {
+
+    state = {
+        counter: 0,
+    }
+
+    constructor(props){
+        super(props)
+
+        setInterval(() => {
+            this.setState((state) => {
+                return {counter: state.counter + 1}
+            })
+        }
+        ,1000)
+    }
+    render() {
+        return <h1>{this.state.counter}</h1>
+    }
+}
