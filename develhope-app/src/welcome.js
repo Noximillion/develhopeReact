597e64337ba391f4657f7b33a7ccc49c2f58ecad
Andre <aanuta9997@gmail.com>
2023-06-02 20:42:32 +0200

    fixed exercise 10

--- develhope-app/src/welcome.js
+++ develhope-app/src/welcome.js
@@ -6,8 +6,11 @@ export class Welcome extends React.Component {
   render() {
     return (
       <div>
-        {this.props.name === 'John' && <p>Welcome, {this.props.name}</p>}
-        {this.props.age > 18 && this.props.age < 65 && <Age age={this.props.age} />}
+        <p>Welcome, {this.props.name}</p>
+        {this.props.name === 'John' 
+          && this.props.age > 18 
+          && this.props.age < 65 
+          && <Age age={this.props.age} />}
       </div>
     );
   }
