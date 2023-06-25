b23630dca0a4902462ce9ab37516e914797b984a
Andre <aanuta9997@gmail.com>
2023-06-25 13:20:40 +0200

    esercizio 51

--- develhope-app/src/UseGithubUser.js
+++ develhope-app/src/UseGithubUser.js
@@ -1,6 +1,5 @@
-// Modify the useGithubUser custom hook from to use the useSWR hook to fetch the data of a Github user.
+// Modify the useGithubUser hook so that, if the username is null, no request is made.
 
-import { useEffect, useState } from "react"
 import useSWR from 'swr' 
 
 const fetcher = url => fetch(url).then(res => res.json())
@@ -8,33 +7,10 @@ const fetcher = url => fetch(url).then(res => res.json())
 
 export function useGetGithubUser(username) {
 
-    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
-
-    // const [data, setData] = useState('')
-    // const [loading, setLoading] = useState(false)
-    // const [error, setError] = useState(null)
-    
-    // useEffect(() => {
-    //     fetch(`https://api.github.com/users/${username}`)
-    //     .then(res => {
-    //         if(res.status !== 200) {
-    //             setError(new Error('costui non esiste'))
-    //         } 
-            
-    //         return res.json()
-    //     })
-    //     .then(json => {
-    //         setData(json)
-    //         setLoading(false)
-    //     })
-    //     .catch(error => {
-    //         setError(error)
-    //     })
-    // }, [username])
+    const { data, error } = useSWR(username && `https://api.github.com/users/${username}`, fetcher)
 
     return {
         data: data, 
         error: error,
-        // loading: loading,
     }
 }
