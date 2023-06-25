74266e3e7f7364a71ea957e03c1518ff68c714e4
Andre <aanuta9997@gmail.com>
2023-06-25 13:16:20 +0200

    esercizio 50

--- develhope-app/src/GithubUser.js
+++ develhope-app/src/GithubUser.js
@@ -5,11 +5,10 @@ export function GetGithubUser() {
 
     const { username } = useParams() 
 
-    const {data, loading, error} = useGetGithubUser(username)
+    const {data, error} = useGetGithubUser(username)
 
     return (
         <div>
-            {loading && <h5>Loading...</h5>}
             {data && <h5>{data.login}<br/>{data.name}</h5>}
             {error && <h5>errore</h5>}
         </div>
--- develhope-app/src/UseGithubUser.js
+++ develhope-app/src/UseGithubUser.js
@@ -1,32 +1,40 @@
+// Modify the useGithubUser custom hook from to use the useSWR hook to fetch the data of a Github user.
+
 import { useEffect, useState } from "react"
+import useSWR from 'swr' 
+
+const fetcher = url => fetch(url).then(res => res.json())
+
 
 export function useGetGithubUser(username) {
 
-    const [data, setData] = useState('')
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+    const { data, error } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+
+    // const [data, setData] = useState('')
+    // const [loading, setLoading] = useState(false)
+    // const [error, setError] = useState(null)
     
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${username}`)
-        .then(res => {
-            if(res.status !== 200) {
-                setError(new Error('costui non esiste'))
-            } 
+    // useEffect(() => {
+    //     fetch(`https://api.github.com/users/${username}`)
+    //     .then(res => {
+    //         if(res.status !== 200) {
+    //             setError(new Error('costui non esiste'))
+    //         } 
             
-            return res.json()
-        })
-        .then(json => {
-            setData(json)
-            setLoading(false)
-        })
-        .catch(error => {
-            setError(error)
-        })
-    }, [username])
+    //         return res.json()
+    //     })
+    //     .then(json => {
+    //         setData(json)
+    //         setLoading(false)
+    //     })
+    //     .catch(error => {
+    //         setError(error)
+    //     })
+    // }, [username])
 
     return {
         data: data, 
         error: error,
-        loading: loading,
+        // loading: loading,
     }
 }
